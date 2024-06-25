b8c908003b5acfac13dcc616ebf0d1a445e35133
Akhil Sanil <99110858+noobmaster2255@users.noreply.github.com>
2024-06-25 16:13:04 -0400

    completed

--- kotlin/TaskManager/taskManager/app/src/main/java/com/akhil/taskmanager/MainActivity.kt
+++ kotlin/TaskManager/taskManager/app/src/main/java/com/akhil/taskmanager/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.akhil.taskmanager
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var addBtn: Button
+    private lateinit var todoRecyclerview: RecyclerView
+    private lateinit var adapter: TodoAdapter
+    private var todoList = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        editText = findViewById(R.id.editText)
+        addBtn = findViewById(R.id.addBtn)
+        todoRecyclerview = findViewById(R.id.todoRecyclerview)
+
+        adapter = TodoAdapter(this, todoList)
+        todoRecyclerview.adapter = adapter
+        todoRecyclerview.layoutManager = LinearLayoutManager(this)
+
+        addBtn.setOnClickListener {
+            val todoTitle = editText.text.toString()
+            if (todoTitle.isNotEmpty()){
+                todoList.add(todoTitle)
+                adapter.notifyItemInserted(todoList.size -1)
+                editText.text.clear()
+            }else Toast.makeText(this, "Task field is required",Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
--- kotlin/TaskManager/taskManager/app/src/main/java/com/akhil/taskmanager/TodoAdapter.kt
+++ kotlin/TaskManager/taskManager/app/src/main/java/com/akhil/taskmanager/TodoAdapter.kt
@@ -1,4 +1,40 @@
 package com.akhil.taskmanager
 
-class TodoAdapter {
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter (private val context: Context, private val todos:List<String>): RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
+
+    inner class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val todoTextTitle:TextView = itemView.findViewById(R.id.todoItemTitle)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.todo_recycler_view_item, parent, false)
+        return  TodoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val todo = todos[position]
+        holder.todoTextTitle.text = todo
+        holder.todoTextTitle.setOnClickListener {
+            val intent = Intent(context, TodoDetailActivity::class.java)
+            intent.putExtra(TODO_TITLE, todo)
+            context.startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val TODO_TITLE = "TODO_TITLE"
+    }
+
 }
